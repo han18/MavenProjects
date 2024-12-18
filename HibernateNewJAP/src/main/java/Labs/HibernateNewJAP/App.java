ad5247b411b0d285a575d815d031e9fa9f88889b
han18 <han.fadah@gmail.com>
2024-12-18 11:22:26 -0500

    completed Glab 305.42A demonstration of hibernate query language added one class findUser and called the methods in main App.java main

--- HibernateNewJAP/src/main/java/Labs/HibernateNewJAP/App.java
+++ HibernateNewJAP/src/main/java/Labs/HibernateNewJAP/App.java
@@ -8,10 +8,29 @@ public class App
 	public static void main( String[] args )
     {
       findUser_Hql u = new findUser_Hql();
-    	u.findUser();
+//    	u.findUser();
     	
     	// calling the select method here from findUser.java class
-    	u.findUserSelect();
+//    	u.findUserSelect();
+      // worked
+    	
+    	// calling the select where clause here in the main method
+//    	c) `WHERE` Clause and Order by Clause
+//    	u.getRecordbyId();
+      // worked
+    	
+    	// d) Multiple SELECT Expressions
+//    	u.getrecords();
+      // worked
+    	
+    	// Example: Aggregate Function calling the method
+//    	u.getmaxSalary();
+      // worked
+      
+     //  f) GROUP BY Clause and Aggregate function calling the method
+      u.getmaxSalaryGroupBy();
+      // last method worked
+
     }
 }
 
--- HibernateNewJAP/src/main/java/com/test/hib/controller/findUser_Hql.java
+++ HibernateNewJAP/src/main/java/com/test/hib/controller/findUser_Hql.java
@@ -39,5 +39,58 @@ public class findUser_Hql {
 
 	      } 
 	    }
+	
+	// this is the recored method where clause
+	public void getRecordbyId() {
+	    SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	   Session session = factory.openSession();
+	   String hql = "FROM User E WHERE E.id > 2 ORDER BY E.salary DESC";
+	   TypedQuery query = session.createQuery(hql);
+	   List<User> results = query.getResultList();
+	   for (User u : results) {
+	     System.out.println("User Id: " +u.getId() + "|" + " Full name:" + u.getFullname() +"|"+ "Email: " + u.getEmail() +"|"+ "password" + u.getPassword());
+	   }
+	}
+	
+	// d) Multiple SELECT Expressions
+	// this method, we will use the multiple columns as shown below:
+	public void getrecords()
+	{
+	   SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	   Session session = factory.openSession();
+	   TypedQuery<Object[]> queryy = session.createQuery(
+	   "SELECT U.salary, U.fullname FROM User AS U", Object[].class);
+	   List<Object[]> resultss = queryy.getResultList();
+	   for (Object[] a : resultss) {
+	       System.out.println("Salary: " + a[0] + ", City: " + a[1]);
+	   }
+	}
+	
+	// e) Example: Aggregate Function
+	// This method executes a SELECT query that returns a single untyped result.
+	public void  getmaxSalary()
+	{
+	   SessionFactory factory = new   Configuration().configure().buildSessionFactory();
+	   Session session = factory.openSession();
+	   String hql = "SELECT max(U.salary) FROM User U";
+	   TypedQuery query = session.createQuery(hql);
+	   double result =(double) query.getSingleResult();
+	   System.out.println(result);
+	}
+
+//   f) GROUP BY Clause and Aggregate function
+	public void   getmaxSalaryGroupBy()
+	{
+	   SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	   Session session = factory.openSession();
+	   String hql = "SELECT SUM(U.salary), U.city FROM User U GROUP BY U.city";
+	   TypedQuery query = session.createQuery(hql);
+	   List<Object[]> result =query.getResultList();
+	   for (Object[] o : result) {
+	       System.out.println("Total salary " +o[0] +" | city: "+ o[1] );
+	   }
+	}
+
+
 }
 
