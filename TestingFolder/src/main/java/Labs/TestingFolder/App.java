2d8fc6154fde627ea4838941ef0aac96cae46718
han18 <han.fadah@gmail.com>
2024-12-19 11:48:37 -0500

    I created a new folder to delete it internally before pushing it to github

---
+++ TestingFolder/src/main/java/Labs/TestingFolder/App.java
@@ -0,0 +1,13 @@
+package Labs.TestingFolder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
