7138ba73a63b0529f6fde01d578a7d0d895ad89d
han18 <han.fadah@gmail.com>
2024-12-20 10:07:30 -0500

    compeleted GLAB - 305.4.3A - Demonstration - Named Queries in Hibernate

---
+++ GLAB-305-4-3ADemonstrationNamedQueries/src/main/java/Labs/GLAB_305_4_3ADemonstrationNamedQueries/App.java
@@ -0,0 +1,19 @@
+package Labs.GLAB_305_4_3ADemonstrationNamedQueries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hello World!" );
+        EmployeController e = new EmployeController();
+        //e.createEmployeeTable();
+        //e.findEmployeeByname();
+        //e.findEmployeeById();
+        e.ShowOfficeCodes_AsDepartment();
+
+    }
+}
---
+++ GLAB-305-4-3ADemonstrationNamedQueries/src/main/java/Labs/GLAB_305_4_3ADemonstrationNamedQueries/EmployeController.java
@@ -0,0 +1,15 @@
+package Labs.GLAB_305_4_3ADemonstrationNamedQueries;
+
+public class EmployeController {
+
+	public void createEmployeeTable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ShowOfficeCodes_AsDepartment() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
